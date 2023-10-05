0a4f9f198ea9fd1789bc17600e8d12304f90a749
ThisuraGallage <tggallage1@gmail.com>
2023-10-05 12:26:27 +0530

    report service added

---
+++ Backend/HotelMIS/src/main/java/com/WHotels/HotelMIS/dto/MonthlyRevenue.java
@@ -0,0 +1,37 @@
+//package com.WHotels.HotelMIS.dto;
+//
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import java.math.BigDecimal;
+//@Setter
+//@NoArgsConstructor
+//public class MonthlyRevenue {
+//    private BigDecimal january;
+//    private BigDecimal february;
+//    private BigDecimal march;
+//    private BigDecimal april;
+//    private BigDecimal may;
+//    private BigDecimal june;
+//    private BigDecimal july;
+//    private BigDecimal august;
+//    private BigDecimal september;
+//    private BigDecimal october;
+//    private BigDecimal november;
+//    private BigDecimal december;
+//
+//    public MonthlyRevenue(BigDecimal january, BigDecimal february, BigDecimal march, BigDecimal april, BigDecimal may, BigDecimal june, BigDecimal july, BigDecimal august, BigDecimal september, BigDecimal october, BigDecimal november, BigDecimal december) {
+//        this.january = january;
+//        this.february = february;
+//        this.march = march;
+//        this.april = april;
+//        this.may = may;
+//        this.june = june;
+//        this.july = july;
+//        this.august = august;
+//        this.september = september;
+//        this.october = october;
+//        this.november = november;
+//        this.december = december;
+//    }
+//}
