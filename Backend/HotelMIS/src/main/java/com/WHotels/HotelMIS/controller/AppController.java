cfcb2667b909f9868c6ed4f96d19e0f4bd98c794
Thulasithan Gnanenthiram <thulasithangnanenthiram@gmail.com>
2023-10-27 12:15:54 +0530

    Added resources and menuitem files again

---
+++ Backend/HotelMIS/src/main/java/com/WHotels/HotelMIS/controller/AppController.java
@@ -0,0 +1,44 @@
+package com.WHotels.HotelMIS.controller;
+
+
+import com.WHotels.HotelMIS.dto.SearchByFiltersResponse;
+import com.WHotels.HotelMIS.model.Room;
+import com.WHotels.HotelMIS.service.AppService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+@CrossOrigin
+public class AppController {
+
+    @Autowired
+    AppService appService;
+
+    @GetMapping("/home")
+    ResponseEntity<com.WHotels.HotelMIS.dto.resort.AppHomeResponse> getHomeScreen() throws Exception{
+        com.WHotels.HotelMIS.dto.resort.AppHomeResponse response = appService.getHomeScreen();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/booking")
+    ResponseEntity<List<SearchByFiltersResponse>> getBookingDetailsByFilters(@RequestParam(required = false) Long bookingId, @RequestParam(required = false)String customerName) throws Exception{
+        List<SearchByFiltersResponse> response = appService.getBookingDetailsByFilters(bookingId, customerName);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/room")
+    ResponseEntity<List<Room>> getRooms(@RequestParam(required = false)Long roomId) throws Exception{
+        List<Room> response = appService.getRooms(roomId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    // create booking - /room/booking - POST
+
+
+
+}
---
+++ Backend/HotelMIS/src/main/java/com/WHotels/HotelMIS/model/MenuItem.java
@@ -0,0 +1,28 @@
+package com.WHotels.HotelMIS.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+@jakarta.persistence.Table(name="menuitem")
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer menuitemId;
+    private String foodType;
+    private String name;
+    private String description;
+    private Integer quantity;
+    private BigDecimal price;
+    private  short discount;
+    private String imageUrl;
+    private BigDecimal rating;
+}
